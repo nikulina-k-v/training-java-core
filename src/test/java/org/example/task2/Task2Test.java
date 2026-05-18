6718d482c14b7f3a380b37970a43642a777ec370
Kristina_N <k.v.nikulina.vrn@gmail.com>
2026-05-18 23:23:19 +0300

    Fix @DisplayName for Task2Test and Task3Test

--- src/test/java/org/example/task2/Task2Test.java
+++ src/test/java/org/example/task2/Task2Test.java
@@ -1,10 +1,12 @@
 package org.example.task2;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Task2Test {
 
     @Test
+    @DisplayName("Задача 2. Вывод ArrayList с сокращением длины элементов до трех символов")
     void printCropArray() {
         Task2.printCropArray(Task2.initColorArray());
     }
--- src/test/java/org/example/task3/Task3Test.java
+++ src/test/java/org/example/task3/Task3Test.java
@@ -1,10 +1,12 @@
 package org.example.task3;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Task3Test {
 
     @Test
+    @DisplayName("Задача 3. Вывод нечетных элементов ArrayList")
     void printArrayWithoutEven() {
         Task3.printArrayWithoutEven(Task3.initColorArray());
     }
