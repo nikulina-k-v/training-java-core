55d730d57309b92e394e09ed0e38ca84c6dc8a92
Kristina_N <k.v.nikulina.vrn@gmail.com>
2026-05-19 01:16:30 +0300

    Task7 is completed

---
+++ src/main/java/org/example/task7/Task7.java
@@ -0,0 +1,69 @@
+package org.example.task7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Task7 {
+    public static ArrayList<String> initColorArrayOne() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Желтый");
+        arrayList.add("Красный");
+        arrayList.add("Белый");
+        arrayList.add("Зеленый");
+        arrayList.add("Черный");
+        arrayList.add("Коричневый");
+        arrayList.add("Синий");
+        arrayList.add("Розовый");
+        arrayList.add("Голубой");
+        arrayList.add("Серый");
+
+        return arrayList;
+    }
+
+    public static ArrayList<String> getRandomColorArrayList () {
+        int itemsCount = (int) (Math.random() * 10);
+        ArrayList<String> referenceList = initColorArrayOne();
+        ArrayList<String> generatedList = new ArrayList<>();
+
+        for (int i = 0; i < itemsCount; i++) {
+            generatedList.add(referenceList.get((int) (Math.random() * 9)));
+        }
+
+        return generatedList;
+    }
+
+    public static void printChecksResult(
+            ArrayList<String> firstArrayList,
+            ArrayList<String> secondArrayList
+    ) {
+        boolean sizeCheck = false;
+        boolean objectsEqualsWithoutOrderCheck = false;
+        boolean fullItemEqualsWithOrderCheck = false;
+
+        if (firstArrayList.size() == secondArrayList.size()) {
+            sizeCheck = true;
+        }
+
+        if (new HashSet<>(secondArrayList).containsAll(new HashSet<>(firstArrayList))) {
+            objectsEqualsWithoutOrderCheck = true;
+        }
+
+        if (firstArrayList.equals(secondArrayList)) {
+            fullItemEqualsWithOrderCheck = true;
+        }
+
+        if (sizeCheck && objectsEqualsWithoutOrderCheck && fullItemEqualsWithOrderCheck) {
+            System.out.println("Списки совпадают!");
+        } else {
+            if (!sizeCheck) {
+                System.out.println("Первая проверка провалена");
+            }
+            if (!objectsEqualsWithoutOrderCheck) {
+                System.out.println("Вторая проверка провалена");
+            }
+            if (!fullItemEqualsWithOrderCheck) {
+                System.out.println("Третья проверка провалена");
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/task7/Task7Test.java
@@ -0,0 +1,91 @@
+package org.example.task7;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class Task7Test {
+
+    @Test
+    @DisplayName("Задача 7. Сравнение двух случайных списков")
+    void printSortedArrayListWithoutDup() {
+        ArrayList<String> firstList = Task7.getRandomColorArrayList();
+        ArrayList<String> secondList = Task7.getRandomColorArrayList();
+
+        System.out.println(">>> First list:");
+        firstList.forEach(System.out::println);
+
+        System.out.println(">>> Second list:");
+        secondList.forEach(System.out::println);
+
+        Task7.printChecksResult(firstList, secondList);
+    }
+
+    @Test
+    @DisplayName("Задача 7. Одинаковый размер, разные элементы")
+    void test1() {
+        ArrayList<String> firstList = new ArrayList<>();
+        firstList.add("Желтый");
+        firstList.add("Красный");
+        firstList.add("Белый");
+
+        ArrayList<String> secondList = new ArrayList<>();
+        secondList.add("Розовый");
+        secondList.add("Голубой");
+        secondList.add("Серый");
+
+        Task7.printChecksResult(firstList, secondList);
+    }
+
+    @Test
+    @DisplayName("Задача 7. Все элементы одного из списков присутствуют во втором списке, вне зависимости от порядка во втором списке (с учетом регистра, white =/= White)")
+    void test2() {
+        ArrayList<String> firstList = new ArrayList<>();
+        firstList.add("Желтый");
+        firstList.add("Красный");
+        firstList.add("Белый");
+
+        ArrayList<String> secondList = new ArrayList<>();
+        secondList.add("Розовый");
+        secondList.add("Желтый");
+        secondList.add("Красный");
+        secondList.add("Белый");
+        secondList.add("Голубой");
+        secondList.add("Серый");
+
+        Task7.printChecksResult(firstList, secondList);
+    }
+
+    @Test
+    @DisplayName("Задача 7. Все элементы одного из списков присутствуют во втором списке в том же порядке (сравнение по индексам, учитывая дубликаты)")
+    void test3() {
+        ArrayList<String> firstList = new ArrayList<>();
+        firstList.add("Желтый");
+        firstList.add("Красный");
+        firstList.add("Белый");
+
+        ArrayList<String> secondList = new ArrayList<>();
+        secondList.add("Желтый");
+        secondList.add("Красный");
+        secondList.add("Белый");
+
+        Task7.printChecksResult(firstList, secondList);
+    }
+
+
+
+//    ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("Желтый");
+//        arrayList.add("Красный");
+//        arrayList.add("Белый");
+//        arrayList.add("Зеленый");
+//        arrayList.add("Черный");
+//        arrayList.add("Коричневый");
+//        arrayList.add("Синий");
+//        arrayList.add("Розовый");
+//        arrayList.add("Голубой");
+//        arrayList.add("Серый");
+//
+//        return arrayList;
+}
